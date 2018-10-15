1fd4a99fe508683caa8410b9ab3d936bed68f5bf
Alexandr Bryukhanov <bryukhanovspb@gmail.com>
2018-10-15 23:21:04 +0300

    axios instances

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -10,7 +10,7 @@ class FullPost extends Component {
     componentDidUpdate() {
         if (this.props.id) {
             if (!this.state.loadPost || (this.state.loadPost && this.state.loadPost.id !== this.props.id)) {
-                axios.get('https://jsonplaceholder.typicode.com/posts/' + this.props.id)
+                axios.get('/posts/' + this.props.id)
                     .then(response => {
                         // console.log(response)
                         this.setState({ loadPost: response.data })
@@ -20,7 +20,7 @@ class FullPost extends Component {
     }
 
     deletePostHandler = () => {
-        axios.delete('https://jsonplaceholder.typicode.com/posts/' + this.props.id)
+        axios.delete('/posts/' + this.props.id)
             .then(response => {
                 console.log(response)
             });
